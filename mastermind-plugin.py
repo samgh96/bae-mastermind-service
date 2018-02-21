e02f825f3fc3ab151c06ec762ecd42165322a0bf
Samuel García <samgh96@gmail.com>
2018-02-21 16:35:45 +0100

    add readme, add url in form and logic in py

--- mastermind-plugin.py
+++ mastermind-plugin.py
@@ -1,5 +1,30 @@
+import json
+import jsonschema
+import yaml
+import requests
+
 from wstore.asset_manager.resource_plugins.plugin import Plugin
 
 
 class MastermindPlugin(Plugin):
-    pass
+
+    _yaml_validator = json.load(open("mastermind-schema.json"))
+
+    def _validate(self, yml, validator):
+        errors = sorted(jsonschema.Draft4Validator(validator).iter_errors(yaml.load(yml)), key=lambda e: e.path)
+        return [x.message for x in errors]
+
+    def on_post_product_offering_validation(self, provider, asset):
+        mastermind = asset.meta_info['configuration_template']
+        err_list = self._validate(mastermind, self._yaml_validator)
+
+        if len(err_list):
+            raise ValueError(' '.join(err_list))
+
+    def on_post_product_spec_attachment(self, asset, asset_t, product_spec):
+        url = asset.meta_info.get('url')
+        headers = {'content-type': 'application/json'}
+        if not url:
+            raise ValueError("URL must be specified")
+
+        requests.put(url, json=product_spec, headers=headers)
